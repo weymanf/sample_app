aaa524618c5cde1124b46a76a006f19e5faf8485
Weyman Fung <wfung0083@yahoo.com>
2013-11-25 17:32:46 -0800

    Make basic User model (including password)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,4 +25,12 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
   end
+
+   describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+end
 end
