053cde26111ef8adcdf813ac2a197a09d2267798
Weyman Fung <wfung0083@yahoo.com>
2013-12-16 17:20:39 -0800

    hey

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ SampleApp::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
-  get "users/new"
+  
   
   
   # The priority is based upon order of creation: first created -> highest priority.
