569a175f972a7dd4a8bfd458451fd1e96b3e6c54
Weyman Fung <wfung0083@yahoo.com>
2013-12-17 16:39:17 -0800

    hey

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
    root  'static_pages#home' 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
